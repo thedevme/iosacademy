3a84c919018158250b7ec9be0e928d5c162e7add
Craig Clayton <craig@thedev.me>
2013-10-05 18:07:24 -0400

    Core Data Start Project

---
+++ week7/RecipeExampleStart01/RecipeExample/IAViewController.h
@@ -0,0 +1,13 @@
+//
+//  IAViewController.h
+//  RecipeExample
+//
+//  Created by Craig Clayton on 10/4/13.
+//  Copyright (c) 2013 iOSAcademy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAViewController : UIViewController
+
+@end
---
+++ week7/RecipeExampleStart01/RecipeExample/IAViewController.m
@@ -0,0 +1,29 @@
+//
+//  IAViewController.m
+//  RecipeExample
+//
+//  Created by Craig Clayton on 10/4/13.
+//  Copyright (c) 2013 iOSAcademy. All rights reserved.
+//
+
+#import "IAViewController.h"
+
+@interface IAViewController ()
+
+@end
+
+@implementation IAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
